c84c76489d6adca5c37c0d530edea5dc48a4aad7
unknown <ahadsupto43@gmail.com>
2025-09-15 23:36:41 +0600

    c

--- assig-base2022/basicsource/scene.cpp
+++ assig-base2022/basicsource/scene.cpp
@@ -8,6 +8,8 @@
 //
 // scene.cpp
 
+
+
 // -----------------------------------------------------------------------------------------
 // includes
 // -----------------------------------------------------------------------------------------
@@ -23,6 +25,7 @@
 #include "fog.h"
 #include "viewfrustum.h"
 
+
 // #includes for geometry
 #include "sweep.h"
 #include "extrusion.h"
@@ -36,14 +39,13 @@
 #include "cylinder.h"
 #include "disc.h"
 
-// Include composite objects
-#include "Composite1.h"
-#include "Composite2.h"
+
 
 // -----------------------------------------------------------------------------------------
 // global variables
 // -----------------------------------------------------------------------------------------
 extern ProgramMode		gProgramMode;							// initially, we're not animating
+
 extern cameraClass		gCamera;								// the camera
 extern object3d			*gSky;									// the global skybox
 extern fog				*gFog;									// the global fog
@@ -53,15 +55,27 @@ extern float			gCurrentFrameTime;						// the time that the current animation fr
 extern vector<object3d*> gShapeVector;
 typedef vector<object3d*>::iterator shapeVectorIterator;
 
+
+
+
+
+
 // -----------------------------------------------------------------------------------------
 // variables that represent 3D objects being used in the animation
 // -----------------------------------------------------------------------------------------
-// Composite objects
-Composite1 *car;
-Composite2 *airplane;
+// these are really global variables, but you shouldn't need to access them from anywere
+// except in this file 
+// -----------------------------------------------------------------------------------------
+
+// pointer of shapes defined here
+
+
+
 
 // some lights
 light *ambient, *light0, *light1, *light2;
+
+
 
 // -----------------------------------------------------------------------------------------
 // constructScene
@@ -78,14 +92,14 @@ void constructScene()
 	light0 = new light(GL_LIGHT0);
 	light1 = new light(GL_LIGHT1);
     
-	// Create composite objects
-	car = new Composite1();
-	airplane = new Composite2();
-	
-	// Position them in the scene
-	car->setPosition(-15, 0, 0);      // Car on the left
-	airplane->setPosition(15, 8, 0);  // Airplane on the right, elevated
+	///Add 3D objects here
+
+
+
 }
+
+
+
 
 // -----------------------------------------------------------------------------------------
 // resetScene
@@ -98,38 +112,32 @@ void constructScene()
 void resetScene()
 {
 	// initialise the camera
-	gCamera.setPosition(0, 6, 25);    // Move camera back to see both objects
-	gCamera.setTarget(0, 3, 0);       // Look at center point between objects
+	gCamera.setPosition(0, 6, 12);
+	gCamera.setTarget(0, 0, 0);
     
 	ambient->setColour(0.5, 0.5, 0.5, 1.0);
     
 	light0->turnOn();
-	light0->setPosition(-10, 15, 10);
-	light0->setColour(0.8, 0.8, 0.8, 1.0);
+	light0->setPosition(-10, 10, 5);
+	light0->setColour(0.7, 0.7, 0.7, 1.0);
 	light0->setSpecularColour(0.7, 0.7, 0.7, 1.0);
 	light0->makePositional();
-	light0->setLinearAttenuation(0.05);
+	light0->setLinearAttenuation(0.1);
 	
 	light1->turnOn();
-	light1->setPosition(10, 15, 10);
-	light1->setColour(0.8, 0.8, 0.8, 1.0);
+	light1->setPosition(10, 10, -5);
+	light1->setColour(0.7, 0.7, 0.7, 1.0);
 	light1->setSpecularColour(0.7, 0.7, 0.7, 1.0);
 	light1->makePositional();
-	light1->setLinearAttenuation(0.05);
+	light1->setLinearAttenuation(0.1);
     
-	// Reset composite objects to their initial state
-	if (car) {
-		car->resetAnimations();
-		car->setPosition(-15, 0, 0);
-		car->show(); // Make sure they're visible
-	}
-	
-	if (airplane) {
-		airplane->resetAnimations();
-		airplane->setPosition(15, 8, 0);
-		airplane->show(); // Make sure they're visible
-	}
+	// reset all objects to their starting position
+
+    
 }
+
+
+
 
 // -----------------------------------------------------------------------------------------
 // animateForNextFrame
@@ -139,30 +147,15 @@ void resetScene()
 
 void animateForNextFrame(float time, long frame)
 {
-	// Animate the car
-	if (car) {
-		car->animateWheelsSpin(kTimeBetweenAnimates);
-		car->animateDoorsOpen(kTimeBetweenAnimates);
-		car->animateCarMovement(kTimeBetweenAnimates);
-	}
-	
-	// Animate the airplane
-	if (airplane) {
-		airplane->animatePropellerSpin(kTimeBetweenAnimates);
-		airplane->animateLandingGearRetract(kTimeBetweenAnimates);
-		airplane->animateFlightPattern(kTimeBetweenAnimates);
-	}
-	
-	// Add some camera movement for a more dynamic view
-	static float cameraAngle = 0.0f;
-	cameraAngle += kTimeBetweenAnimates * 10.0f; // 10 degrees per second
-	
-	float cameraX = 25.0f * cos_d(cameraAngle);
-	float cameraZ = 25.0f * sin_d(cameraAngle);
-	gCamera.setPosition(cameraX, 8, cameraZ);
-	gCamera.setTarget(0, 3, 0);
+
+	////Animate 3D objects here
+
+
 	
 	// eventually stop
 	if (time >= 45)
 		gProgramMode = kpmFinished;
+
 }
+
+
